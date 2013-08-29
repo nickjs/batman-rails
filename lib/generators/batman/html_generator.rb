ec2787db44e79cfa0f9bb9c36154c081da82a192
Nick Small <nick@shopify.com>
2013-08-29 14:52:45 -0400

    Don't validate HTML file names.

--- lib/generators/batman/html_generator.rb
+++ lib/generators/batman/html_generator.rb
@@ -8,21 +8,6 @@ module Batman
       desc "This generator creates a the HTML files needed for a Batman.Controller"
       argument :actions, :type => :array, :default => [], :banner => "action action"
 
-
-      RESERVED_JS_WORDS = %w{
-        break case catch continue debugger default delete do else finally for
-        function if in instanceof new return switch this throw try typeof var void while with
-      }
-
-      def validate_no_reserved_words
-        actions.each do |action|
-          if RESERVED_JS_WORDS.include? action
-             raise Thor::Error, "The name '#{action}' is reserved by javascript " <<
-                                "Please choose an alternative action name and run this generator again."
-          end
-        end
-      end
-
       def create_directory
         with_app_name do
           empty_directory "#{app_path}/html/#{plural_name.downcase}"
